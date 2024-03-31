0bbc2084197c76ee8a597a64ec028e4a19fd9f55
mcodog <markandrie.codog@tup.edu.ph>
2024-03-31 12:44:06 +0800

    SALONGA

--- resources/views/admin/equipments/index.blade.php
+++ resources/views/admin/equipments/index.blade.php
@@ -1,5 +1,26 @@
 @extends('admin.shared.layouts')
+<style>
+        body {
+            background-image: url('{{ asset("images/background9.jpg") }}');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-color: #49243E; /* Change the background color as needed */
+            color: white; /* Change text color to white */
+        }
 
+        .transparent-container {
+            background-color: #C6EBC5; /* Set background color with alpha value for transparency */
+            padding: 10px; /* Add padding for better visibility */
+            border-radius: 10px; /* Add border radius for rounded corners */
+        }
+
+        .transparent-card {
+            background-color: transparent !important; /* Set card background color to transparent */
+            border: none; /* Remove card border */
+            color: #E178C5; /* Change card text color to white */
+        }
+    </style>
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/admin/manufacturers/edit.blade.php
+++ resources/views/admin/manufacturers/edit.blade.php
@@ -1,6 +1,28 @@
 @extends('admin.shared.layouts')
+<style>
+        body {
+            background-image: url('{{ asset("images/background13.jpg") }}');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-color: #49243E; /* Change the background color as needed */
+            color: white; /* Change text color to white */
+        }
 
+        .transparent-container {
+            background-color: #C6EBC5; /* Set background color with alpha value for transparency */
+            padding: 10px; /* Add padding for better visibility */
+            border-radius: 10px; /* Add border radius for rounded corners */
+        }
+
+        .transparent-card {
+            background-color: transparent !important; /* Set card background color to transparent */
+            border: none; /* Remove card border */
+            color: #E178C5; /* Change card text color to white */
+        }
+    </style>
 @section('content')
+<div class="container mt-5" style="background-color: rgba(239, 188, 155, 0.7); background-size: cover; background-position: center;">
 <div class="container mt-5">
     <h1>Edit Manufacturer</h1>
     <form enctype="multipart/form-data" action="{{ url('manufacturer/'. $manufacturer->id .'/update') }}" method="post" class="mt-4">
