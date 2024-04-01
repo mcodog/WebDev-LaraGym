cc21edc7ddc99814327bf094cc6428b775e8f926
mcodog <gelgin2001@gmail.com>
2024-04-02 06:03:45 +0800

    gelg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,35 @@
 @extends('admin.shared.layouts')
 
 <style>
-    body {
-    position: relative; /* Ensure proper stacking order */
-    background-image: url('{{ asset('images/background1.jpg') }}');
-    background-size: cover;
-    background-position: center;
-    background-attachment: fixed;
-}
 
-body::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.3); /* Adjust opacity here */
-    z-index: -1; /* Place behind the content */
-}
+    h1 {
+        text-align: center; /* Center the text horizontally */            
+        color: white !important; /* Set text color to white */
+        font-size: 36px; /* Set the font size */
+        margin-top: 100px; /* Add space above the h1 */
+        margin-bottom: 50px; /* Add space below the h1 */
+    }
+
+
+
+    body {
+        position: relative; /* Ensure proper stacking order */
+        background-image: url('{{ asset('images/background1.jpg') }}');
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+    }
+
+    body::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.3); /* Adjust opacity here */
+        z-index: -1; /* Place behind the content */
+    }
 
     .transparent-card {
         background-image: rgba(255, 255, 255, 0); /* Set background image */
@@ -44,9 +55,12 @@ body::before {
         box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.3); /* Add box shadow */
         transform: translateY(-5px); /* Elevate effect */
     }
+
 </style>
 
 @section('content')
+<h1> WELCOME TO FITZONE </h1> 
+
 <div class="container card-container">
     <div class="row justify-content-center">
         <div class="col-md-6 mb-5">
